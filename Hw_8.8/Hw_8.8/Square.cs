bcd6dcafab3bda5aada6fa7b56eca393ae8878d9
basarYargici <ibrahimbyargici@gmail.com>
2021-06-01 23:25:02 +0300

    Square and SquareTest implmented.

---
+++ Hw_8.8/Hw_8.8/Square.cs
@@ -0,0 +1,24 @@
+﻿namespace HW_8._8
+{
+    class Square
+    {
+        private double _side;
+
+        public double Side { get => _side; set => _side = value; }
+
+        public Square()
+        {
+        }
+
+        public Square(double Side)
+        {
+            this.Side = Side;
+        }
+
+        public override string ToString()
+        {
+            return "Square side = " + _side;
+        }
+    }
+
+}
---
+++ Hw_8.8/Hw_8.8/SquareTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW_8._8
+{
+    class SquareTest
+    {
+        static void Main(string[] args)
+        {
+            // test square class without argument
+            Square firstSquare = new Square();
+            firstSquare.Side = 3.5;
+            Console.WriteLine(firstSquare.ToString());
+
+            // test square class side argument
+            Square secondSquare = new Square(34);
+            Console.WriteLine(secondSquare.ToString());
+
+            // to stop console disappearing in debug mode
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+    }
+}
